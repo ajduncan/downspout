7c614b83c36a2527a5d2fb277199bc4af7824313
Andy Duncan <andy@duncaningram.com>
2014-12-14 17:08:12 -0500

    Cleaned up the example program and packagized downspout.

--- ds.py
+++ ds.py
@@ -2,11 +2,7 @@
 
 import os
 
-from downspout.settings import *
-from downspout.bandcamp import *
-from downspout.soundcloud import *
-from downspout.youtube import *
-from downspout.vimeo import *
+from downspout import bandcamp, settings, soundcloud, youtube, vimeo
 
 
 def fetch_all(filename):
@@ -14,14 +10,14 @@ def fetch_all(filename):
     for media in records:
         [app, artist] = [item.strip() for item in media.split(',')]
         if app.lower() == 'soundcloud':
-            soundcloud_fetch_media(
-                '{0}/{1}'.format(SOUNDCLOUD_FRONT_URL, artist))
+            soundcloud.soundcloud_fetch_media(
+                '{0}/{1}'.format(settings.SOUNDCLOUD_FRONT_URL, artist))
         if app.lower() == 'bandcamp':
-            bandcamp_fetch_media(artist)
+            bandcamp.bandcamp_fetch_media(artist)
         if app.lower() == 'youtube':
-            youtube_fetch_media(artist)
+            youtube.youtube_fetch_media(artist)
         if app.lower() == 'vimeo':
-            vimeo_fetch_media(artist)
+            vimeo.vimeo_fetch_media(artist)
 
 
 if __name__ == "__main__":
